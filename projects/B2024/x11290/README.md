f15bb219dfb24154ed1bfa8c358917e00f16c0bf
DejvB <david.bainar@gmail.com>
2026-06-19 17:50:47 +0200

    images

---
+++ projects/B2024/x11290/README.md
@@ -0,0 +1,11 @@
+# Micro:bit Python pictures
+
+Simple pictures project made with Micro:bit and Pyhton.
+
+## Author: 
+
+x11290
+
+## Description
+
+Pictures created in Micro:bit and Python program.
---
+++ projects/B2024/x11290/images.py
@@ -0,0 +1,29 @@
+# Imports go at the top
+from microbit import *
+
+
+# Code in a 'while True:' loop repeats forever
+while True:
+    display.show(Image('09090:'
+                       '99999:'
+                       '99999:'
+                       '09990:'
+                       '00900'))
+    sleep(3000)
+    display.show(Image('00900:'
+                       '03330:'
+                       '33333:'
+                       '09990:'
+                       '09990'))
+    sleep(3000)
+    display.show(Image('09090:'
+                       '90909:'
+                       '90009:'
+                       '09090:'
+                       '00900'))
+    sleep(3000)
+    
+        
+    
+    
+    

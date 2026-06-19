30dd64d3c8356048ad02757493a0d0b8863bbb3a
DejvB <david.bainar@gmail.com>
2026-06-19 18:35:13 +0200

    image gallery project

---
+++ projects/B2024/x11294/README.md
@@ -0,0 +1,9 @@
+# Micro:bit galerie obrázků
+ Tento program ukazuje různé obrázky.
+
+## Autorka
+  Veronika Vyhlídalová
+
+## Popis
+ Pythonový program v micro:bitu, co ukazuje galerii obrázků. 
+
---
+++ projects/B2024/x11294/image gallery.py
@@ -0,0 +1,62 @@
+# Imports go at the top
+from microbit import *
+
+
+# Code in a 'while True:' loop repeats forever
+while True:
+    display.show(Image('00900:'
+                       '09090:'
+                       '90909:'
+                       '00900:'
+                       '09900'))
+    sleep(1000)
+     
+    display.show(Image('09990:'
+                       '98889:'
+                       '90809:'
+                       '96069:'
+                       '09990'))
+    sleep(1000)
+
+    display.show(Image('00900:'
+                       '00990:'
+                       '00969:'
+                       '09900:'
+                       '09900'))
+    sleep(1000)
+    
+    display.show(Image('00300:'
+                       '03630:'
+                       '36963:'
+                       '03630:'
+                       '00300'))
+    sleep(1000)
+    
+    display.show(Image('00000:'
+                       '09090:'
+                       '00000:'
+                       '90009:'
+                       '09990'))
+    sleep(1000)
+
+    display.show(Image('99000:'
+                       '09000:'
+                       '09009:'
+                       '09990:'
+                       '09090'))
+    sleep(1000)
+
+    display.show(Image('03930:'
+                       '09690:'
+                       '03930:'
+                       '00990:'
+                       '00900'))
+    sleep(1000)
+    
+    display.show(Image('09990:'
+                       '99999:'
+                       '09990:'
+                       '00900:'
+                       '00000'))
+    
+    sleep(1000)
